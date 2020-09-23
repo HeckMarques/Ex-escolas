31c9d7db90f9d762dd757a2659f679186130f6f2
MauricioMarques3809 <mauricio.marques888@gmail.com>
2020-09-23 15:03:43 -0300

    Updates

---
+++ precache-manifest.8f2c165d8cb5ba26c18b062c81d7d455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4205e39c54df5f844c5d7510b36d82e7",
+    "url": "/Ex-escolas/index.html"
+  },
+  {
+    "revision": "75eeaae1136a969f4b05",
+    "url": "/Ex-escolas/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d5ef87cf491bdfa04c2",
+    "url": "/Ex-escolas/static/js/2.1e5c2fd3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Ex-escolas/static/js/2.1e5c2fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eeaae1136a969f4b05",
+    "url": "/Ex-escolas/static/js/main.fd8e6f1d.chunk.js"
+  },
+  {
+    "revision": "4f1e6a2f0de01e0115d6",
+    "url": "/Ex-escolas/static/js/runtime-main.8553af4a.js"
+  }
+]);
